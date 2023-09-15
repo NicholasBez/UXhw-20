ebf18dfed1c5fb29a96f78f6b218f4373aca24b2
NicholasBez <129118288+NicholasBez@users.noreply.github.com>
2023-09-14 21:56:44 -0500

    added case study HTML and javascript to link to the specified html

--- js/script.js
+++ js/script.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
     $("#myButton").click(function() {
         alert("Button clicked!");
     })
+
+    $("#caseStudy").click(function() {
+        if (confirm("Click ok to view case study")) {
+            document.location.href = ("./caseStudy.html")
+        }
+        
+    })
 })
